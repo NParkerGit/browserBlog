1643232c0aa99165333126ad5518010ab8044f0b
NParkerGit <nickp@wightman.ca>
2020-09-22 21:39:54 -0400

    image formatting

--- items/index.js
+++ items/index.js
@@ -1,10 +1,10 @@
 export default [
     
-    {"id": "item1", "title":"NY Health", "fname":"NYHealth.md", "specialImage":"/img/NYHealthPortfolio.png"},
-    {"id": "item2", "title":"RecruitR", "fname":"RecruitR.md", "specialImage":"/img/RecruitRPortfolio.png"},
-    {"id": "item3", "title":"Skyview", "fname":"Skyview.md", "specialImage":"/img/SkyviewPortfolio.png"},
-    {"id": "item4", "title":"Tech4All", "fname":"T4A.md", "specialImage":"/img/Tech4All.png"},
-    {"id": "item5", "title":"SEV", "fname":"SEV.md", "specialImage":"/img/SEVPortfolio.png"},
-    {"id": "item6", "title":"ElliotLake", "fname":"ElliotLake.md", "specialImage":"/img/ElliotLakePortfolio.png"},
-    {"id": "item7", "title":"LDNP", "fname":"LDNP.md", "specialImage":"/img/LDNPPortfolio.png"}
+    {"id": "item1", "title":"NY Health", "fname":"NYHealth.md", "specialImage":"./img/NYHealthPortfolio.png"},
+    {"id": "item2", "title":"RecruitR", "fname":"RecruitR.md", "specialImage":"./img/RecruitRPortfolio.png"},
+    {"id": "item3", "title":"Skyview", "fname":"Skyview.md", "specialImage":"./img/SkyviewPortfolio.png"},
+    {"id": "item4", "title":"Tech4All", "fname":"T4A.md", "specialImage":"./img/Tech4All.png"},
+    {"id": "item5", "title":"SEV", "fname":"SEV.md", "specialImage":"./img/SEVPortfolio.png"},
+    {"id": "item6", "title":"ElliotLake", "fname":"ElliotLake.md", "specialImage":"./img/ElliotLakePortfolio.png"},
+    {"id": "item7", "title":"LDNP", "fname":"LDNP.md", "specialImage":"./img/LDNPPortfolio.png"}
 ];
